4481a7fc9ac866d8f11f794c923de5e03bb4f57e
Millenia <86996848+miwllen@users.noreply.github.com>
2021-07-14 23:44:15 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'regenerator-runtime/runtime';
+import data from './album/album.js';
+
+console.log(process.env.REACT_APP_CLIENT_ID);
+
+function showAlert(){
+  alert(`You choose: ${data.album.name} as your fav!`)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 className="topNav">Pop Hits</h1>
+        <h1 className="topNav">It's 90s</h1>
+        <h1 className="topNav">New Song</h1>
+      </div>
+    
+      <h1 className="songsTitle">Music You Might Like</h1>
+      <div className="itemContainer">
+      <input type="image" src={data.album.images[1].url} alt="img" />
+        <p className="songTitle">{data.album.name}</p>
+        <p className="artist">{data.album.artists[0].name}</p>
+        <button className="selectButton" onClick={showAlert}>Select</button>
+      </div>
     </div>
   );
 }
